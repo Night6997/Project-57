e4611918546de788906332eda6df47cd23728716
Night6997 <83875915+Night6997@users.noreply.github.com>
2021-05-14 12:32:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function ShowInfoAbout() {
+window.alert("Gaming is really a workout for your mind disguised as fun. Studies have shown that playing video games regularly may increase gray matter in the brain and boost brain connectivity. (Gray matter is associated with muscle control, memories, perception, and spatial navigation.)I like to play a lot of games. I started playing a lot of games from august 2020.Its fun and has a lot of puzzles for me to figure out which i like.");
+}
+function ShowInfo() {
+window.alert("My favorite game is minecraft i play it a lot with my friends");
+}
+function ShowInfoContact(){
+window.alert("You can contact me by emailing me on sourishkrishna45@gmail.com");
+}
